01fd933ce21768eea5c40180ac1310ab904b939c
Kashinath Gopal Jeure <kashinathjeure@gmail.com>
2022-11-24 22:32:46 +0530

    Variables
    Variables

---
+++ 01_variables.ts
@@ -0,0 +1,62 @@
+var num: number = 1;
+console.log(num);
+
+
+var str1: string = " I am angular Developer";
+var str2: string = "power full people come form powerful places";
+
+console.log(str1);
+console.log(str2);
+
+var isMarried: boolean = true;
+
+var youMarried: boolean = false;
+
+console.log(isMarried);
+console.log(youMarried);
+
+
+var a1: any = {
+    productID: 1,
+    ProductName: 'iPhone',
+    productPrice: 25000
+}
+
+console.log(a1);
+
+// homodenous array 
+var arr1 : Array<string> = ['Angular', 'React', 'NodeJS', 'java'];
+
+// heterogenous array 
+var arr2 : Array<any> = ['Angular', 123456, true];
+
+
+console.log(arr2);
+
+
+
+
+var myNum: number = 20;
+var myStr: string = "thank";
+var myBoo:boolean =true;
+
+console.log(myNum);
+console.log(myStr);
+console.log(myBoo);
+
+
+var greet: string = "Hii Good Morning";
+
+console.log(greet);
+
+var number: number = 100;
+console.log(number);
+
+var greet2: string = "hii Good Night";
+console.log(greet2);
+
+
+
+
+
+
