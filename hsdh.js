058f7794714520dbd0d771d2e46d17380356ade6
Kashinath Gopal Jeure <kashinathjeure@gmail.com>
2022-11-26 20:37:23 +0530

    Practice
    Practice

---
+++ hsdh.js
@@ -0,0 +1,18 @@
+console.log("=========Assignment Interface==========");
+var IEmployeeInfo = {
+    idNumber: '154667',
+    name: "Mallikarjun Mali",
+    company: "Wipro",
+    department: "IT Angulardeveloper",
+    occupation: "Engineer",
+    email: 'malluroyal@gmail.com',
+    city: "Pune"
+};
+for (var key in IEmployeeInfo) {
+    if (Object.prototype.hasOwnProperty.call(IEmployeeInfo, key)) {
+        var element = IEmployeeInfo[key];
+        console.log("".concat(key, " : ").concat(element));
+    }
+}
+
+
