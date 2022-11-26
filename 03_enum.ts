53245d918bd0b3836b6ccfb12b9fc5d68245b4fb
Kashinath Gopal Jeure <kashinathjeure@gmail.com>
2022-11-26 20:25:03 +0530

    Enum
    Enum

---
+++ 03_enum.ts
@@ -0,0 +1,42 @@
+enum Gender {
+    Male,
+    Female
+}
+
+console.log(Gender.Female);
+console.log(Gender.Male);
+console.log(Gender[1]);
+console.log(Gender[0]);
+
+
+enum WeekEnds {
+    Saturday = 6,
+    Sunday = 7
+}
+
+console.log(WeekEnds[6]);
+console.log(WeekEnds[7]);
+
+enum daysInWeek {
+    Monday = 1,
+    Tuesday = 2,
+    Thursday = 3,
+    Wednesday =4,
+    Friday =5,
+    Saturday =6,
+    Sunday =7
+}
+
+console.warn("++++++++++++++++ Days in Week ++++++++++++++++");
+
+
+console.log(daysInWeek[1]);
+console.log(daysInWeek[2]);
+console.log(daysInWeek[3]);
+console.log(daysInWeek[4]);
+console.log(daysInWeek[5]);
+console.log(daysInWeek[6]);
+console.log(daysInWeek[7]);
+console.log(daysInWeek.Monday);
+
+
