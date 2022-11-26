050ffc4113be621fd0cac75eeefbce1f1579bac6
Kashinath Gopal Jeure <kashinathjeure@gmail.com>
2022-11-26 20:35:09 +0530

    Class
    Class

---
+++ 10_class.ts
@@ -0,0 +1,20 @@
+class Passenger {
+    firstName:string;
+    lastName: string;
+    frequentFlyerno: number
+    constructor(firstName: string, lastName:string, flightno:number) 
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.frequentFlyerno = flightno;
+    }
+    display() {
+        console.log(this.firstName + " " + this.lastName + " "+ this.frequentFlyerno);
+    }
+
+}
+
+var passenger = new Passenger("Jsck", "Sparrow", 102);
+
+
+
